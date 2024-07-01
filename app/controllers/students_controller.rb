cbc688f047ff270b37df820332c51afee8047280
Luis Eduardo Plácido Guimarães <luisegsilva.le@gmail.com>
2024-07-01 07:36:48 -0300

    feat: list students

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,6 +1,10 @@
 class  StudentsController < ApplicationController
 
-
+  def index()
+      students_all = Student.all;
+      selected_students = chosse_students(page_params,students_all)
+      render json: { data:selected_students },status: :ok
+  end
 
   def create
     student = Student.new(student_params)
@@ -15,5 +19,20 @@ class  StudentsController < ApplicationController
   private
   def student_params
     params.permit(:name, :cpf, :birthdate, :payment_method)
+  end
+
+  def page_params
+        params.permit(:page, :count)
+  end
+
+  def chosse_students(page_params,students_all)
+    index_max = page_params["page"] * page_params["count"]
+    index_min = index_max - page_params["count"]
+    selected_students = []
+
+    for student in index_min...index_max
+      selected_students.append(students_all[student])
+    end
+    selected_students
   end
 end
